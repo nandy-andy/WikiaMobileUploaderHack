0acdbdd0f519d87336c26e1da8df128a805a9e34
nandy-andy <nandy@wikia-inc.com>
2012-12-14 11:25:20 +0100

    simplified app.js

--- Resources/app.js
+++ Resources/app.js
@@ -1,12 +1,4 @@
 (function() {
-	var User = require('modules/User');
-	var Logger = require('modules/Logger');
-	var WikiaApp = {
-		user: new User(),
-		logger: new Logger()
-	};
-	
-	var ApplicationWindow = require('ui/ApplicationWindow');
-	var app = new ApplicationWindow(WikiaApp);
-	app.init();
+	var ApplicationWindow = require('ui/ApplicationWindow').ApplicationWindow;
+	new ApplicationWindow().init();
 })();
